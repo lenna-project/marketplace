51394ce8ce655c5defaa812c33514b02fa7c17c6
chriamue <chriamue@users.noreply.github.com>
2021-04-30 08:48:44 +0000

    deploy: f09aa0f73a826b75201ff4346990de395fa56bf2

---
+++ 911.js
@@ -0,0 +1,263 @@
+(self["webpackChunk_lenna_project_marketplace"] = self["webpackChunk_lenna_project_marketplace"] || []).push([[911],{
+
+/***/ 251:
+/***/ (() => {
+
+// extracted by mini-css-extract-plugin
+    if(false) { var cssReload; }
+  
+
+/***/ }),
+
+/***/ 616:
+/***/ (() => {
+
+// extracted by mini-css-extract-plugin
+    if(false) { var cssReload; }
+  
+
+/***/ }),
+
+/***/ 824:
+/***/ (() => {
+
+// extracted by mini-css-extract-plugin
+    if(false) { var cssReload; }
+  
+
+/***/ }),
+
+/***/ 26:
+/***/ (() => {
+
+// extracted by mini-css-extract-plugin
+    if(false) { var cssReload; }
+  
+
+/***/ }),
+
+/***/ 911:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXTERNAL MODULE: consume shared module (default) vue@^3.0.11 (strict) (fallback: ./node_modules/@vue/runtime-dom/dist/runtime-dom.esm-bundler.js)
+var runtime_dom_esm_bundler_js_ = __webpack_require__(748);
+;// CONCATENATED MODULE: ./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[1]!./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[4].use[0]!./src/App.vue?vue&type=template&id=41b88c34
+
+
+function render(_ctx, _cache, $props, $setup, $data, $options) {
+  const _component_Navbar = (0,runtime_dom_esm_bundler_js_.resolveComponent)("Navbar")
+  const _component_router_view = (0,runtime_dom_esm_bundler_js_.resolveComponent)("router-view")
+
+  return ((0,runtime_dom_esm_bundler_js_.openBlock)(), (0,runtime_dom_esm_bundler_js_.createBlock)(runtime_dom_esm_bundler_js_.Fragment, null, [
+    (0,runtime_dom_esm_bundler_js_.createVNode)(_component_Navbar),
+    (0,runtime_dom_esm_bundler_js_.createVNode)(_component_router_view)
+  ], 64))
+}
+;// CONCATENATED MODULE: ./src/App.vue?vue&type=template&id=41b88c34
+
+;// CONCATENATED MODULE: ./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[1]!./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[4].use[0]!./src/components/Navbar.vue?vue&type=template&id=174c0c93&scoped=true
+
+const _withId = /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.withScopeId)("data-v-174c0c93")
+
+;(0,runtime_dom_esm_bundler_js_.pushScopeId)("data-v-174c0c93")
+const _hoisted_1 = { class: "navbar" }
+const _hoisted_2 = /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.createTextVNode)("Home")
+const _hoisted_3 = /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.createVNode)("a", { href: "https://github.com/lenna-project" }, "Github", -1)
+const _hoisted_4 = /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.createTextVNode)("About")
+;(0,runtime_dom_esm_bundler_js_.popScopeId)()
+
+const Navbarvue_type_template_id_174c0c93_scoped_true_render = /*#__PURE__*/_withId((_ctx, _cache, $props, $setup, $data, $options) => {
+  const _component_router_link = (0,runtime_dom_esm_bundler_js_.resolveComponent)("router-link")
+
+  return ((0,runtime_dom_esm_bundler_js_.openBlock)(), (0,runtime_dom_esm_bundler_js_.createBlock)("div", _hoisted_1, [
+    (0,runtime_dom_esm_bundler_js_.createVNode)(_component_router_link, { to: "/" }, {
+      default: _withId(() => [
+        _hoisted_2
+      ]),
+      _: 1
+    }),
+    _hoisted_3,
+    (0,runtime_dom_esm_bundler_js_.createVNode)(_component_router_link, { to: "/about" }, {
+      default: _withId(() => [
+        _hoisted_4
+      ]),
+      _: 1
+    })
+  ]))
+})
+;// CONCATENATED MODULE: ./src/components/Navbar.vue?vue&type=template&id=174c0c93&scoped=true
+
+;// CONCATENATED MODULE: ./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[4].use[0]!./src/components/Navbar.vue?vue&type=script&lang=js
+
+/* harmony default export */ const Navbarvue_type_script_lang_js = ({
+  name: "Navbar",
+});
+
+;// CONCATENATED MODULE: ./src/components/Navbar.vue?vue&type=script&lang=js
+ 
+// EXTERNAL MODULE: ./node_modules/mini-css-extract-plugin/dist/loader.js??clonedRuleSet-2.use[0]!./node_modules/css-loader/dist/cjs.js!./node_modules/vue-loader/dist/stylePostLoader.js!./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[4].use[0]!./src/components/Navbar.vue?vue&type=style&index=0&id=174c0c93&scoped=true&lang=css
+var Navbarvue_type_style_index_0_id_174c0c93_scoped_true_lang_css = __webpack_require__(251);
+;// CONCATENATED MODULE: ./src/components/Navbar.vue?vue&type=style&index=0&id=174c0c93&scoped=true&lang=css
+
+;// CONCATENATED MODULE: ./src/components/Navbar.vue
+
+
+
+
+;
+Navbarvue_type_script_lang_js.render = Navbarvue_type_template_id_174c0c93_scoped_true_render
+Navbarvue_type_script_lang_js.__scopeId = "data-v-174c0c93"
+
+/* harmony default export */ const Navbar = (Navbarvue_type_script_lang_js);
+;// CONCATENATED MODULE: ./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[4].use[0]!./src/App.vue?vue&type=script&lang=js
+
+
+
+/* harmony default export */ const Appvue_type_script_lang_js = ((0,runtime_dom_esm_bundler_js_.defineComponent)({
+  name: "Marketplace",
+  components: {
+    Navbar: Navbar,
+  },
+}));
+
+;// CONCATENATED MODULE: ./src/App.vue?vue&type=script&lang=js
+ 
+;// CONCATENATED MODULE: ./src/App.vue
+
+
+
+Appvue_type_script_lang_js.render = render
+
+/* harmony default export */ const App = (Appvue_type_script_lang_js);
+// EXTERNAL MODULE: consume shared module (default) vue-router@^4.0.6 (strict) (fallback: ./node_modules/vue-router/dist/vue-router.esm-bundler.js)
+var vue_router_esm_bundler_js_ = __webpack_require__(755);
+;// CONCATENATED MODULE: ./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[1]!./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[4].use[0]!./src/views/Home.vue?vue&type=template&id=d081d7d4&scoped=true
+
+const Homevue_type_template_id_d081d7d4_scoped_true_withId = /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.withScopeId)("data-v-d081d7d4")
+
+;(0,runtime_dom_esm_bundler_js_.pushScopeId)("data-v-d081d7d4")
+const Homevue_type_template_id_d081d7d4_scoped_true_hoisted_1 = { class: "main" }
+;(0,runtime_dom_esm_bundler_js_.popScopeId)()
+
+const Homevue_type_template_id_d081d7d4_scoped_true_render = /*#__PURE__*/Homevue_type_template_id_d081d7d4_scoped_true_withId((_ctx, _cache, $props, $setup, $data, $options) => {
+  return ((0,runtime_dom_esm_bundler_js_.openBlock)(), (0,runtime_dom_esm_bundler_js_.createBlock)("div", Homevue_type_template_id_d081d7d4_scoped_true_hoisted_1))
+})
+;// CONCATENATED MODULE: ./src/views/Home.vue?vue&type=template&id=d081d7d4&scoped=true
+
+;// CONCATENATED MODULE: ./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[4].use[0]!./src/views/Home.vue?vue&type=script&lang=js
+
+
+/* harmony default export */ const Homevue_type_script_lang_js = ((0,runtime_dom_esm_bundler_js_.defineComponent)({
+  name: "Home",
+  components: {},
+  data() {
+    return {};
+  },
+}));
+
+;// CONCATENATED MODULE: ./src/views/Home.vue?vue&type=script&lang=js
+ 
+// EXTERNAL MODULE: ./node_modules/mini-css-extract-plugin/dist/loader.js??clonedRuleSet-2.use[0]!./node_modules/css-loader/dist/cjs.js!./node_modules/vue-loader/dist/stylePostLoader.js!./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[4].use[0]!./src/views/Home.vue?vue&type=style&index=0&id=d081d7d4&scoped=true&lang=css
+var Homevue_type_style_index_0_id_d081d7d4_scoped_true_lang_css = __webpack_require__(824);
+;// CONCATENATED MODULE: ./src/views/Home.vue?vue&type=style&index=0&id=d081d7d4&scoped=true&lang=css
+
+;// CONCATENATED MODULE: ./src/views/Home.vue
+
+
+
+
+;
+Homevue_type_script_lang_js.render = Homevue_type_template_id_d081d7d4_scoped_true_render
+Homevue_type_script_lang_js.__scopeId = "data-v-d081d7d4"
+
+/* harmony default export */ const Home = (Homevue_type_script_lang_js);
+;// CONCATENATED MODULE: ./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[1]!./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[4].use[0]!./src/views/About.vue?vue&type=template&id=243cff65&scoped=true
+
+const Aboutvue_type_template_id_243cff65_scoped_true_withId = /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.withScopeId)("data-v-243cff65")
+
+;(0,runtime_dom_esm_bundler_js_.pushScopeId)("data-v-243cff65")
+const Aboutvue_type_template_id_243cff65_scoped_true_hoisted_1 = { class: "about" }
+const Aboutvue_type_template_id_243cff65_scoped_true_hoisted_2 = /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.createVNode)("h1", null, "About Lenna Project", -1)
+const Aboutvue_type_template_id_243cff65_scoped_true_hoisted_3 = /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.createVNode)("p", null, " Lenna is a library for image processing algorithms. The web page allows to test and use them. ", -1)
+const Aboutvue_type_template_id_243cff65_scoped_true_hoisted_4 = /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.createVNode)("h2", null, "Plugins", -1)
+const _hoisted_5 = /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.createVNode)("p", null, [
+  /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.createTextVNode)(" Lenna allows to plug in more algorithms. Some examples can be found at "),
+  /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.createVNode)("a", { href: "https://github.com/lenna-project/lenna-plugins" }, "lenna-project/lenna-plugins"),
+  /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.createTextVNode)(" on github. ")
+], -1)
+const _hoisted_6 = /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.createVNode)("h2", null, "Command Line Interface", -1)
+const _hoisted_7 = /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.createVNode)("p", null, [
+  /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.createTextVNode)(" A command line interface allows to use the algorithms and run them on multiple images at once. The source for "),
+  /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.createVNode)("a", { href: "https://github.com/lenna-project/lenna-cli" }, "lenna-cli"),
+  /*#__PURE__*/(0,runtime_dom_esm_bundler_js_.createTextVNode)(" can be found on github. ")
+], -1)
+;(0,runtime_dom_esm_bundler_js_.popScopeId)()
+
+const Aboutvue_type_template_id_243cff65_scoped_true_render = /*#__PURE__*/Aboutvue_type_template_id_243cff65_scoped_true_withId((_ctx, _cache) => {
+  return ((0,runtime_dom_esm_bundler_js_.openBlock)(), (0,runtime_dom_esm_bundler_js_.createBlock)("div", Aboutvue_type_template_id_243cff65_scoped_true_hoisted_1, [
+    Aboutvue_type_template_id_243cff65_scoped_true_hoisted_2,
+    Aboutvue_type_template_id_243cff65_scoped_true_hoisted_3,
+    Aboutvue_type_template_id_243cff65_scoped_true_hoisted_4,
+    _hoisted_5,
+    _hoisted_6,
+    _hoisted_7
+  ]))
+})
+;// CONCATENATED MODULE: ./src/views/About.vue?vue&type=template&id=243cff65&scoped=true
+
+// EXTERNAL MODULE: ./node_modules/mini-css-extract-plugin/dist/loader.js??clonedRuleSet-2.use[0]!./node_modules/css-loader/dist/cjs.js!./node_modules/vue-loader/dist/stylePostLoader.js!./node_modules/vue-loader/dist/index.js??ruleSet[1].rules[4].use[0]!./src/views/About.vue?vue&type=style&index=0&id=243cff65&scoped=true&lang=css
+var Aboutvue_type_style_index_0_id_243cff65_scoped_true_lang_css = __webpack_require__(616);
+;// CONCATENATED MODULE: ./src/views/About.vue?vue&type=style&index=0&id=243cff65&scoped=true&lang=css
+
+;// CONCATENATED MODULE: ./src/views/About.vue
+
+const script = {}
+
+;
+script.render = Aboutvue_type_template_id_243cff65_scoped_true_render
+script.__scopeId = "data-v-243cff65"
+
+/* harmony default export */ const About = (script);
+;// CONCATENATED MODULE: ./src/router.js
+
+
+
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+];
+
+const router = (0,vue_router_esm_bundler_js_.createRouter)({
+  history: (0,vue_router_esm_bundler_js_.createWebHistory)(),
+  routes,
+});
+
+/* harmony default export */ const src_router = (router);
+// EXTERNAL MODULE: ./src/index.css
+var src = __webpack_require__(26);
+;// CONCATENATED MODULE: ./src/bootstrap.js
+
+
+
+
+
+(0,runtime_dom_esm_bundler_js_.createApp)(App).use(src_router).mount("#app");
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=911.js.map
