396f9c37666703e1b32eed7b8028d755f13b407e
Christian M <chriamue@gmail.com>
2021-08-29 17:01:54 +0200

    :recycle: :fire: removed about page and use about from lenna web

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "@/views/Home.vue";
-import About from "@/views/About.vue";
+import About from "@lenna-project/lenna-web/src/views/About.vue";
 
 const routes = [
   {
